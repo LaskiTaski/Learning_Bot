3bf84eb92b9933202ce39c3456afb39c58e09070
Krasnikov Artem <dgj.vap.11@mail.ru>
2023-12-13 17:45:14 +0300

    Fixed imports

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,6 +1,4 @@
 from keyboards import kb_FSM
 from keyboards import kb_HomeWork
-from keyboards import kb_Learning
-from keyboards import kb_Materials
 from keyboards import kb_Project
 from keyboards import kb_Video
